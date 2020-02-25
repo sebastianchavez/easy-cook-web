474211e26db21b127a2663a211c5883d28acc854
seba <seba.chavez21@gmail.com>
2020-02-25 15:21:43 -0300

    cambios

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './App.css';
+import Navigation from './components/navigation/navigation';
+import Carousel from './components/carousel/carousel'
+import Home from './pages/home/home';
+
+function App() {
+  return (
+    <Router>
+      <div className="site-wrap">
+      <Navigation />
+      <Carousel />
+      <div class="slant-1"></div>
+
+        <Route path="/" component={Home} exact />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -3,9 +3,28 @@ import React, { Component } from 'react'
 export default class carousel extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment className="slide-one-item home-slider owl-carousel">
+
+                <div className="site-blocks-cover inner-page overlay hero1"  data-aos="fade" data-stellar-background-ratio="0.5">
+                    <div className="container">
+                        <div className="row align-items-center justify-content-center">
+                            <div className="col-md-6 text-center" data-aos="fade">
+                                <h1 className="font-secondary  font-weight-bold text-uppercase">Welcome to Colorlib</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="site-blocks-cover inner-page overlay hero2"  data-aos="fade" data-stellar-background-ratio="0.5">
+                    <div className="container">
+                        <div className="row align-items-center justify-content-center">
+                            <div className="col-md-7 text-center" data-aos="fade">
+                                <h1 className="font-secondary font-weight-bold text-uppercase">Free Bootstrap 4 Templates</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
